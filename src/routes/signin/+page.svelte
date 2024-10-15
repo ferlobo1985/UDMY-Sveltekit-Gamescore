c86730e1983ee66b5735f2fb1d7d03781906a697
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-15 17:51:34 -0300

    04-Signin resgister form
    04-Signin resgister form

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,1 +1,65 @@
-<h1>Sign in</h1>
+<script>
+    import { createForm } from 'felte';
+    import { validator } from '@felte/validator-yup';
+    import * as yup from 'yup';
+
+    let formType = false;
+    let loader = false;
+
+    const {form, errors} = createForm({
+        onSubmit:(values)=>{
+            handleSubmit(values)
+        }
+    });
+
+
+    const handleSubmit = (values) => {
+        loader = true;
+        if(formType){
+            // REGISTER
+        } else {
+            // SIGNIN
+        }
+    }
+</script>
+
+<div class="row justify-content-md-center">
+    <div class="col col-lg-5">
+        <h1>{formType ? 'Register':'Sign in'}</h1>
+        <hr/>
+        <form use:form>
+
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input 
+                    type="email"
+                    name="email"
+                    class="form-control"
+                    value="francis@gmail.com"
+                >
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input 
+                    type="password"
+                    name="password"
+                    class="form-control"
+                    value="testing123"
+                >
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+                Submit
+            </button>
+        </form>
+        <hr/>
+        <button
+            class="btn btn-outline-primary"
+            on:click={()=>formType = !formType}
+        >
+            {formType ? 'Want to sign in ?':'Want to register ?'}
+        </button>
+
+    </div>
+</div>
