e108f09aba5b7240ad7c1230db2a03b528ea7971
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-15 17:19:07 -0300

    03-Main layout and nav
    03-Main layout and nav

---
+++ src/lib/components/Navigation/Nav.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<header>
+	<div class="container">
+		<header class="d-flex flex-wrap justify-content-center py-3 mb-2 border-bottom">
+			<a
+				href="/"
+				class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+			>
+				<span class="fs-4">Gamescore</span>
+			</a>
+
+			<ul class="nav nav-pills">
+				<li class="nav-item">
+					<a href="/" class="nav-link"
+                        class:active={$page.url.pathname === '/'}
+                    >Home</a>
+				</li>
+				
+				<li class="nav-item">
+					<button class="nav-link">Logout</button>
+				</li>
+				<li class="nav-item">
+					<a href="/dashboard" class="nav-link"
+                        class:active={$page.url.pathname === '/dashboard'}
+                    >Dashboard</a>
+				</li>
+				<li class="nav-item">
+					<a href="/signin" class="nav-link"
+                        class:active={$page.url.pathname === '/signin'}
+                    >Sign in</a>
+				</li>
+			</ul>
+		</header>
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Nav from "$lib/components/Navigation/Nav.svelte";
+</script>
+
+<main>
+    <Nav/>
+    <div class="container">
+        <slot/>
+    </div>
+</main>
