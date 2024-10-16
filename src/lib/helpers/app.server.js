60e3044a1d67b28d73aee117065c2b652fb42d47
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-16 17:59:33 -0300

    16-Server hooks part two
    16-Server hooks part two

--- src/lib/helpers/app.server.js
+++ src/lib/helpers/app.server.js
@@ -1,4 +1,5 @@
 import { AUTH } from "$lib/firebase/server/firebase.server"
+import { redirect } from "@sveltejs/kit";
 
 
 export const getAuthUser = async(userToken) => {
@@ -17,6 +18,21 @@ export const getAuthUser = async(userToken) => {
 }
 
 export const guardedRoutes = async(event) => {
-    console.log(event.url.pathname);
-    console.log(event.locals.user)
+    // console.log(event.url.pathname);
+    // console.log(event.locals.user)
+    
+    /// SIGN IN ROUTES
+    if(event.url.pathname === '/signin'){
+        if(event.locals.user){
+            throw redirect(302,'/')
+        }
+    }
+
+    ///AUTH GUARD
+    if(event.url.pathname.match('/dashboard')){
+        if(!event.locals.user){
+            throw redirect(302,'/signin')
+        }
+    }
+
 }
