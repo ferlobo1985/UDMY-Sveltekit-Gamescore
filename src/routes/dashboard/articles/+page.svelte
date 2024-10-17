e72833bbc10325e61efff0aa074b6bc03e7d16b0
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-17 01:24:54 -0300

    23-User articles table
    23-User articles table

--- src/routes/dashboard/articles/+page.svelte
+++ src/routes/dashboard/articles/+page.svelte
@@ -1,7 +1,36 @@
 <script>
+    //@ts-nocheck
+    import { goto } from "$app/navigation";
     export let data;
 </script>
 
-<div>
-    <h1>Articles</h1>
-</div>
+
+<h1>Articles</h1>
+<table class="table table-dark">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Game</th>
+            <th scope="col">Title</th>
+            <th scope="col">Edit</th>
+        </tr>
+    </thead>
+    <tbody>
+    {#each data.articles as article,index (article.id)}
+         <tr>
+            <th scope="row">{index+1}</th>
+            <th>{article.game}</th>
+            <td><a href="/article/{article.id}" class="link-info">{article.title}</a></td>
+            <td>
+                <button
+                    class="btn btn-outline-warning btn-sm"
+                    on:click={()=> goto(`/dashboard/edit/${article.id}`)}
+                >
+                    Edit article
+                </button>
+            </td>
+
+        </tr>
+    {/each}
+    </tbody>
+</table>
